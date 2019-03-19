bdd35d3c996012838f88bc5a095c28337b263422
Joe Bailey <Joseph.bailey@hmrcaspire.com>
2019-03-19 14:00:11 +0000

    base libgdx project

---
+++ desktop/src/com/td/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.td.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.td.game.TowerDefenseGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TowerDefenseGame(), config);
+	}
+}
