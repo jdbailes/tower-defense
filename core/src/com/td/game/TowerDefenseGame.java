c1dc3dbcaa3beda94eedd7b80e7bfb92557f2564
Joe Bailey <Joseph.bailey@hmrcaspire.com>
2019-03-19 15:34:29 +0000

    basic screen with alien that moves across the screen

--- core/src/com/td/game/TowerDefenseGame.java
+++ core/src/com/td/game/TowerDefenseGame.java
@@ -3,31 +3,62 @@ package com.td.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.awt.Rectangle;
 
 public class TowerDefenseGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+  private OrthographicCamera camera;
+  private SpriteBatch batch;
+
+  private Rectangle alien;
+  private Texture alienImage;
+
+  private static final String ALIEN_IMG = "alien.png";
+
+  @Override
+  public void create() {
+    // Load the images for the alien
+    alienImage = new Texture(Gdx.files.internal(ALIEN_IMG));
+
+    camera = new OrthographicCamera();
+    camera.setToOrtho(false, 1600, 960);
+
+    batch = new SpriteBatch();
+
+    alien = new Rectangle();
+    alien.x = 20;
+    alien.y = 800;
+    alien.width = 64;
+    alien.height = 64;
+  }
+
+  @Override
+  public void render() {
+    Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    camera.update();
+
+    alien.x += 75 * Gdx.graphics.getDeltaTime();
+
+    if (alien.x < 0) {
+      alien.x = 0;
+    }
+    if (alien.x > 1600 - 64) {
+      alien.x = 1600 - 64;
+    }
+
+    batch.setProjectionMatrix(camera.combined);
+    batch.begin();
+    batch.draw(alienImage, alien.x, alien.y);
+    batch.end();
+  }
+
+  @Override
+  public void dispose() {
+    batch.dispose();
+  }
 }
--- desktop/src/com/td/game/desktop/DesktopLauncher.java
+++ desktop/src/com/td/game/desktop/DesktopLauncher.java
@@ -5,8 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.td.game.TowerDefenseGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new TowerDefenseGame(), config);
-	}
+
+  public static void main(String[] arg) {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+    // Manual config override
+    config.title = "Tower Defense";
+    config.width = 1600;
+    config.height = 960;
+
+    new LwjglApplication(new TowerDefenseGame(), config);
+  }
 }
