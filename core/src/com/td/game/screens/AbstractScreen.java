ff202114e3733c4f3e88249fe519827d65a0a639
Joe Bailey <Joseph.bailey@hmrcaspire.com>
2019-06-26 19:43:06 +0100

    Added level previews to the level menu screen

--- core/src/com/td/game/screens/AbstractScreen.java
+++ core/src/com/td/game/screens/AbstractScreen.java
@@ -29,7 +29,8 @@ public abstract class AbstractScreen implements Screen {
 
   @Override
   public void render(float delta) {
-    Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+    // Sets background colour to black
+    Gdx.gl.glClearColor(0, 0, 0, 1);
     Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
   }
